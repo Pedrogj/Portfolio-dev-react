c9277217c727b93cba7ff93de9977a2f8dfaeb7b
Pedro Perez <pedrodrums2015@gmail.com>
2025-11-15 12:16:31 -0300

    Add projects structure basic

--- src/components/ProjectsSection/Projects.tsx
+++ src/components/ProjectsSection/Projects.tsx
@@ -1,3 +1,21 @@
+import imgProject from "../../../public/images/project-img.jpg";
+
 export const Projects = () => {
-  return <div>Projects</div>;
+  return (
+    <div className="project-container">
+      <h1 className="project-title">Proyectos</h1>
+      <div className="project-card-wrapper">
+        <div className="project-card">
+          <img src={imgProject} alt="img-project" />
+          <h2>Name Project</h2>
+          <ul>
+            <li>ReactJs</li>
+            <li>React Router</li>
+            <li>Context Api</li>
+          </ul>
+          <a href="#">Ver más</a>
+        </div>
+      </div>
+    </div>
+  );
 };
