2031e5b02f840f4c20c1007af9bb0efa3dcbcfc3
Pedro Perez <pedrodrums2015@gmail.com>
2025-11-15 13:48:34 -0300

    Add styles in project component

--- src/components/ProjectsSection/Projects.tsx
+++ src/components/ProjectsSection/Projects.tsx
@@ -6,14 +6,47 @@ export const Projects = () => {
       <h1 className="project-title">Proyectos</h1>
       <div className="project-card-wrapper">
         <div className="project-card">
-          <img src={imgProject} alt="img-project" />
-          <h2>Name Project</h2>
-          <ul>
-            <li>ReactJs</li>
-            <li>React Router</li>
-            <li>Context Api</li>
-          </ul>
-          <a href="#">Ver más</a>
+          <img className="img-project" src={imgProject} alt="img-project" />
+          <div className="project-info-content">
+            <h2 className="project-card-title">Name Project</h2>
+            <ul className="project-item-wrapper">
+              <li className="project-item-tecnology">ReactJs</li>
+              <li className="project-item-tecnology">React Router</li>
+              <li className="project-item-tecnology">Context Api</li>
+              <li className="project-item-tecnology">Context Api</li>
+            </ul>
+            <a className="button-link" href="#">
+              Ver más
+            </a>
+          </div>
+        </div>
+        <div className="project-card">
+          <img className="img-project" src={imgProject} alt="img-project" />
+          <div className="project-info-content">
+            <h2 className="project-card-title">Name Project</h2>
+            <ul className="project-item-wrapper">
+              <li className="project-item-tecnology">ReactJs</li>
+              <li className="project-item-tecnology">React Router</li>
+              <li className="project-item-tecnology">Context Api</li>
+            </ul>
+            <a className="button-link" href="#">
+              Ver más
+            </a>
+          </div>
+        </div>
+        <div className="project-card">
+          <img className="img-project" src={imgProject} alt="img-project" />
+          <div className="project-info-content">
+            <h2 className="project-card-title">Name Project</h2>
+            <ul className="project-item-wrapper">
+              <li className="project-item-tecnology">ReactJs</li>
+              <li className="project-item-tecnology">React Router</li>
+              <li className="project-item-tecnology">Context Api</li>
+            </ul>
+            <a className="button-link" href="#">
+              Ver más
+            </a>
+          </div>
         </div>
       </div>
     </div>
